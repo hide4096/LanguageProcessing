e5caff35d37eaa22d58c2c59710dfb2e5151f3bb
hide4096 <asouhide2002@gmail.com>
2022-11-23 09:22:44 +0900

    [update]簡便法による第二近似できた

---
+++ problem4_simple_two.c
@@ -0,0 +1,60 @@
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<ctype.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+char* GenerateSimple2(char* filename,int len){
+    int fd = open(filename,O_RDONLY);
+
+    struct stat st;
+    if(fstat(fd,&st) == -1) return NULL;
+    char* removed = (char*)malloc((size_t)st.st_size);
+    if(removed == NULL) return NULL;
+
+    long size = 0;
+    for(long i=0;i<st.st_size;i++){
+        char c;
+        read(fd,&c,1);
+        if(isalpha(c) != 0 || c == '\n' || c == ' '){
+            removed[size] = c;
+            size++;
+        }
+    }
+
+    srand(time(NULL));
+    char* generated = (char*)malloc(len);
+    if(removed == NULL) return NULL;
+
+    srand(time(NULL));
+    long r = size*((float)rand()/RAND_MAX);
+    char A = removed[r];
+    generated[0] = A;
+    for(int i=1;i<len;i++){
+        r = size*((float)rand()/RAND_MAX);
+        while(removed[r] != A){
+            r++;
+            if(r>=size-1) r = size*((float)rand()/RAND_MAX);
+        }
+        A = removed[r+1];
+        generated[i] = A;
+    }
+
+    free(removed);
+    close(fd);
+    return generated;
+}
+
+
+int main(){
+    char* generated = GenerateSimple2("./Airplane",1000);
+    if(generated == NULL) return -1;
+
+    printf("%s\r\n",generated);
+
+    free(generated);
+    return 0;
+}
