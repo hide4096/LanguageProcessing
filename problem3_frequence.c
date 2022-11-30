3f157ed9679487a828abc74fa2e86b181192c36b
hide4096 <asouhide2002@gmail.com>
2022-11-30 09:28:45 +0900

    [update]二つ組、三つ組の出現頻度の出力追加

--- problem3_frequence.c
+++ problem3_frequence.c
@@ -8,51 +8,49 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int* FreqCharaEx(char* filename){
+int** FreqCharaEx(char* filename){
     int fd = open(filename,O_RDONLY);
     
     //アルファベットと空白と改行
-    int* freq = (int*)calloc(sizeof(int),28*sizeof(int));
+    int** retfreq = (int**)calloc(sizeof(int),3*sizeof(int*));
+    retfreq[0] = (int*)calloc(sizeof(int),28*sizeof(int));
+    retfreq[1] = (int*)calloc(sizeof(int),28*28*sizeof(int));
+    retfreq[2] = (int*)calloc(sizeof(int),28*28*28*sizeof(int));
 
-    int** freq2 = (int**)calloc(sizeof(int),28*sizeof(int*));
-    for(int i=0;i<28;i++) freq2[i] = (int*)calloc(sizeof(int),28*sizeof(int));
-    
-    int*** freq3 = (int***)calloc(sizeof(int),28*sizeof(int**));
-    for(int i=0;i<28;i++){
-        freq3[i] = (int**)calloc(sizeof(int),28*sizeof(int*));
-        for(int j=0;j<28;j++) freq3[i][j] = (int*)calloc(sizeof(int),28*sizeof(int));
-    }
+    for(int i=0;i<3;i++) if(retfreq[i] == NULL) return NULL;
 
     struct stat st;
     if(fstat(fd,&st) == -1) return NULL;
 
-    char past_i,ppast_i;
+    char past_id,ppast_id;
 
     for(off_t i=0;i<st.st_size;i++){
         char c;
-        char i;
+        char id;
         read(fd,&c,1);
-        if(isupper(c) != 0)         i=c-'A';
-        else if(islower(c) != 0)    i=c-'a';
-        else if(c == ' ')           i=26;
-        else if(c == '\n')          i=27;
+        if(isupper(c) != 0)         id=c-'A';
+        else if(islower(c) != 0)    id=c-'a';
+        else if(c == ' ')           id=26;
+        else if(c == '\n')          id=27;
 
-        freq[i]++;
+        retfreq[0][id]++;
 
-        if(i <= 0) freq2[past_i][i]++;
-        if(i <= 1) freq3[ppast_i][past_i][i]++;
-        ppast_i = past_i;
-        past_i = i;
+        if(i > 0) retfreq[1][28*past_id+id]++;
+        if(i > 1) retfreq[2][28*28*ppast_id+28*past_id+id]++;
+        ppast_id = past_id;
+        past_id = id;
     }
 
     close(fd);
-    return freq;
+    return retfreq;
 }
 
 
 int main(){
-    int* freq = FreqCharaEx("./hamlet");
-    if(freq == NULL) return -1;
+    int** retfreq = FreqCharaEx("./hamlet");
+    if(retfreq == NULL) return -1;
+
+    int* freq = retfreq[0];
 
     char chara[28];
     for(int i=0;i<26;i++) chara[i] = 'a'+i;
@@ -72,7 +70,66 @@ int main(){
         }
     }
 
+    int* freq2 = retfreq[1];
+
+    char chara2[28*28][2];
+    for(int i=0;i<28;i++){
+        for(int j=0;j<28;j++){
+            chara2[i*28+j][0] = chara[i];
+            chara2[i*28+j][1] = chara[j];
+        }
+    }
+
+    for(int i=0;i<28*28-1;i++){
+        for(int j=0;j<28*28-1-i;j++){
+            if(freq2[j] < freq2[j+1]){
+                int tmp_int = freq2[j];
+                freq2[j] = freq2[j+1];
+                freq2[j+1] = tmp_int;
+                for(int k=0;k<2;k++){
+                    char tmp_char = chara2[j][k];
+                    chara2[j][k] = chara2[j+1][k];
+                    chara2[j+1][k] = tmp_char;
+                }
+            }
+        }
+    }
+
+    int* freq3 = retfreq[2];
+
+    char chara3[28*28*28][3];
+
+    for(int i=0;i<28;i++){
+        for(int j=0;j<28;j++){
+            for(int k=0;k<28;k++){
+                int t = i*28*28+j*28+k;
+                chara3[t][0] = chara[i];
+                chara3[t][1] = chara[j];
+                chara3[t][2] = chara[k];
+            }
+        }
+    }
+
+    for(int i=0;i<28*28*28-1;i++){
+        for(int j=0;j<28*28*28-1-i;j++){
+            if(freq3[j] < freq3[j+1]){
+                int tmp_int = freq3[j];
+                freq3[j] = freq3[j+1];
+                freq3[j+1] = tmp_int;
+                for(int k=0;k<3;k++){
+                    char tmp_char = chara3[j][k];
+                    chara3[j][k] = chara3[j+1][k];
+                    chara3[j+1][k] = tmp_char;
+                }
+            }
+        }
+    }
+    printf("1つ組\r\n");
     for(int i=0;i<28;i++) printf("%c|%d\r\n",chara[i],freq[i]);
+    printf("2つ組\r\n");
+    for(int i=0;i<30;i++) printf("%c%c|%d\r\n",chara2[i][0],chara2[i][1],freq2[i]);
+    printf("3つ組\r\n");
+    for(int i=0;i<30;i++) printf("%c%c%c|%d\r\n",chara3[i][0],chara3[i][1],chara3[i][2],freq3[i]);
 
     free(freq);
     return 0;
